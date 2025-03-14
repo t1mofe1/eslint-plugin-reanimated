767ad9615a8f7c2d94affbd37116869df05782c0
Timothy Myagi <timofei.magi@gmail.com>
2025-03-15 01:08:03 +0200

    Added eslint doc generator config

---
+++ .eslint-doc-generatorrc.js
@@ -0,0 +1,10 @@
+/** @type {import('eslint-doc-generator').GenerateOptions} */
+const config = {
+	"configEmoji": [["recommended", "✅"]],
+	"ignoreConfig": ["recommended", "flat/recommended"],
+	"urlConfigs": "https://github.com/t1m0fe1/eslint-plugin-reanimated#-configs",
+	"ruleDocSectionOptions": false,
+}
+
+// eslint-disable-next-line no-undef
+module.exports = config;
